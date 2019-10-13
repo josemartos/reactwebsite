dcd16d1fddca20652aa6a192ff129dd49a503f4b
Jose <martosjosele@gmail.com>
2019-10-13 18:00:14 +0200

    htaccess to dist root

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Index = () => (
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Front-end Developer | Jose Martos</title>
 
-      <link rel="shortcut icon" href="/assets/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
     <PageRoot>
       <Home />

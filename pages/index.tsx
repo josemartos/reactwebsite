947ddaa760b4ec67c165b7593eb054700afb3db1
Jose Martos <jose.martos@flyla.de>
2020-02-28 13:35:57 +0100

    Update CV

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const HomePage: NextPage = () => (
     <div className="home-info">
       <h1 className="home-title">Hi, I am Jos&eacute; Martos</h1>
       <div className="home-position">
-        front-end developer and design advocate at{' '}
+        front-end developer at{' '}
         <a href="https://www.flyla.com/" title="Visit Flyla's website">
           Flyla
         </a>
